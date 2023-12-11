b0eb3e345672e2ded65877a5c50af1f4e972db48
maswiecho <maswiecho@gmail.com>
2023-12-11 16:58:59 +0100

    bugfix

--- tree.js
+++ tree.js
@@ -11,39 +11,15 @@ const rl = readline.createInterface({
 readline.emitKeypressEvents(process.stdin);
 if (process.stdin.isTTY) process.stdin.setRawMode(true);
 
-// Function to create the base Christmas tree
-function createChristmasTree(height) {
-  let tree = [];
-  for (let i = 0; i < height; i++) {
-    let stars = "*".repeat(2 * i + 1);
-    let spaces = " ".repeat(height - i - 1);
-    tree.push(spaces + stars);
-  }
-  tree.push(" ".repeat(height - 1) + "|");
-  return tree;
-}
-
-// Function to add sparkle effect to the tree
-function sparkleChristmasTree(tree) {
-  return tree.map((line) => {
-    return line
-      .split("")
-      .map((char) => {
-        return Math.random() > 0.9 ? "o" : char;
-      })
-      .join("");
-  });
-}
-
-// Function to display the tree in the console
-function displayTree(tree) {
+// Function to display the timer
+function displayTimer(seconds) {
   console.clear();
-  tree.forEach((line) => console.log(line));
+  console.log("Timer: " + seconds + "s");
 }
 
 // User input handling
 let userInput = "";
-console.log("Enter your text: ");
+console.log("Enter your text (press Enter to save): ");
 
 process.stdin.on("keypress", (str, key) => {
   if (key && key.ctrl && key.name === "c") {
@@ -52,17 +28,21 @@ process.stdin.on("keypress", (str, key) => {
     fs.writeFileSync("user_input.txt", userInput);
     console.log("\nYour text has been saved to user_input.txt");
     process.exit();
+  } else if (key && key.name === "backspace") {
+    // Handle backspace
+    userInput = userInput.slice(0, -1);
   } else {
     userInput += str;
   }
 });
 
-const height = 10; // Height of the Christmas tree
-let christmasTree = createChristmasTree(height);
+let seconds = 0;
 
-// Start an interval to continuously update the tree
+// Start an interval to continuously update the timer
 setInterval(() => {
-  let sparklingTree = sparkleChristmasTree(christmasTree);
-  displayTree(sparklingTree);
+  displayTimer(seconds++);
+  // Move cursor to the beginning of the line and clear it
+  readline.cursorTo(process.stdout, 0, 1);
+  readline.clearLine(process.stdout, 1);
   process.stdout.write("Enter your text: " + userInput); // Display user input
-}, 500);
+}, 1000);
